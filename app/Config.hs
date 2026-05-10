1b3cf4fd40e50ef833e6077b22baa48e9244d3af
ruzen42 <malledeferlive@gmail.com>
2026-05-11 00:28:53 +0500

    feat(add reverse)

--- app/Config.hs
+++ app/Config.hs
@@ -1,33 +1,33 @@
 {-# LANGUAGE DeriveGeneric #-}
 
-module Config (Config(..), stdCfg, execConfig) where 
+module Config (Config(..), stdCfg, execConfig) where
 
 import GHC.Generics (Generic)
 import Data.Aeson (ToJSON, FromJSON)
-import System.Process 
+import System.Process
 import Control.Exception (try, IOException)
 
-data Config = Config 
+data Config = Config
     { options  :: [String]
-    , javaPath :: String 
+    , javaPath :: String
     , maxRam   :: Int
     , startRam :: Int
     , jarPath  :: String
     , guiMode  :: Bool
     } deriving Generic
 
-instance Show Config where 
+instance Show Config where
     show :: Config -> String
-    show cfg = (javaPath cfg) 
-        ++ " " 
-        ++ "-Xms" 
+    show cfg = (javaPath cfg)
+        ++ " "
+        ++ "-Xms"
         ++ (show (startRam cfg)) ++ "G "
         ++ "-Xmx"
         ++ (show (maxRam cfg)) ++ "G"
         ++ optionToStr (options cfg)
         ++ " -jar " ++ jarPath cfg
         ++ if guiMode cfg then " " else " nogui "
-        where  
+        where
           optionToStr :: [String] -> String
           optionToStr (x:xs) = " -XX:" ++ x ++ " " ++ optionToStr xs
           optionToStr [] = ""
@@ -35,20 +35,20 @@ instance Show Config where
 instance ToJSON   Config
 instance FromJSON Config
 
-stdCfg :: Config 
-stdCfg = Config 
+stdCfg :: Config
+stdCfg = Config
     { options  = []
     , javaPath = "/usr/bin/java"
     , guiMode  = False
     , maxRam   = 2
-    , startRam = 2 
-    , jarPath  = "paper.jar"
+    , startRam = 2
+    , jarPath  = "purpur.jar"
     }
 
 execConfig :: Config -> IO ()
-execConfig cfg = do 
+execConfig cfg = do
   let cmd = shell $ show cfg
   raw <- try $ readCreateProcess cmd "" :: IO (Either IOException String)
-  case raw of 
-    Right a -> putStrLn a 
+  case raw of
+    Right a -> putStrLn a
     Left  e -> putStrLn $ "Java Error: " ++ show e
--- app/Main.hs
+++ app/Main.hs
@@ -1,54 +1,55 @@
+
 module Main (main) where
 
-import System.Environment (getArgs)
-import Config (stdCfg, execConfig)
-import System.Directory (doesFileExist)
-import System.IO (hSetBuffering, stdout, BufferMode(NoBuffering))
-import Data.Char (toLower)
-import Control.Monad (unless)
-import System.Exit (exitSuccess, exitFailure)
-import Data.Aeson (decode)
+import System.Environment   (getArgs)
+import Config               (stdCfg, execConfig)
+import Install              (getAvailableVersions, downloadLatestPurpur)
+import System.Directory     (doesFileExist)
+import System.IO            (hSetBuffering, stdout, BufferMode(NoBuffering))
+import Data.Char            (toLower)
+import Control.Monad        (unless)
+import System.Exit          (exitSuccess, exitFailure)
+import Data.Aeson           (decode)
 import Data.Aeson.Encode.Pretty (encodePretty)
 import qualified Data.ByteString.Lazy as BL
-import Install (getAvailableVersions, downloadLatestPurpur)
 
 main :: IO ()
 main = do
     hSetBuffering stdout NoBuffering
     args <- getArgs
 
-    when (not null args && head args == "init") $ do
-        versions <- getAvailableVersions
-        forM_ (zip [0..] versions) $ \(i, v) -> do
-            putStrLn $ show i ++ ": " ++ show v
+    case args of
+        ["--versions"] -> do
+            vs <- getAvailableVersions
+            mapM_ putStrLn vs
 
-        input <- getLine
-        downloadLatestPurpur $ versions !! (read input :: Int)
-        exitSuccess
+        ["--download"] -> do
+            downloadLatestPurpur
 
-    let configPath = if null args then "./mine.cfg" else head args
+        _ -> do
+            let configPath = if null args then "./mc.cfg" else head args
 
-    exists <- doesFileExist configPath
+            exists <- doesFileExist configPath
 
-    unless exists $ do
-        putStr $ "File " ++ configPath ++ " does not exist. Create new? [Y/n]: "
-        answer <- getLine
-        let response = map toLower answer
+            unless exists $ do
+                putStr $ "File " ++ configPath ++ " does not exist. Create new? [Y/n]: "
+                answer <- getLine
+                let response = map toLower answer
 
-        if (response /= "n") then do
-          BL.writeFile configPath (encodePretty stdCfg)
-          putStrLn $ "Created new config at: " ++ configPath
-        else
-          exitSuccess
+                if response /= "n" then do
+                    BL.writeFile configPath (encodePretty stdCfg)
+                    putStrLn $ "Created new config at: " ++ configPath
+                else
+                    exitSuccess
 
-    putStrLn $ "Using config: " ++ configPath
+            putStrLn $ "Using config: " ++ configPath
 
-    file <- BL.readFile configPath
+            file <- BL.readFile configPath
 
-    case decode file of
-      Nothing  -> do
-        putStrLn "Config parsing error"
-        exitFailure
-      Just cfg -> do
-        print cfg
-        execConfig cfg
+            case decode file of
+                Nothing  -> do
+                    putStrLn "Config parsing error"
+                    exitFailure
+                Just cfg -> do
+                    print cfg
+                    execConfig cfg
