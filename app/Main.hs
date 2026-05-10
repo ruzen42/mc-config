9748e60e896a931535aec62d4d72a78d4e08c9ed
ruzen42 <malledeferlive@gmail.com>
2026-05-11 00:16:53 +0500

    feat()

--- app/Main.hs
+++ app/Main.hs
@@ -8,41 +8,47 @@ import Data.Char (toLower)
 import Control.Monad (unless)
 import System.Exit (exitSuccess, exitFailure)
 import Data.Aeson (decode)
-import Data.Aeson.Encode.Pretty (encodePretty) 
+import Data.Aeson.Encode.Pretty (encodePretty)
 import qualified Data.ByteString.Lazy as BL
+import Install (getAvailableVersions, downloadLatestPurpur)
 
 main :: IO ()
-main = do 
+main = do
     hSetBuffering stdout NoBuffering
     args <- getArgs
-    
-    let configPath = if null args then "./mine.cfg" else head args 
-    
+
+    when (not null args && head args == "init") $ do
+        versions <- getAvailableVersions
+        forM_ (zip [0..] versions) $ \(i, v) -> do
+            putStrLn $ show i ++ ": " ++ show v
+
+        input <- getLine
+        downloadLatestPurpur $ versions !! (read input :: Int)
+        exitSuccess
+
+    let configPath = if null args then "./mine.cfg" else head args
+
     exists <- doesFileExist configPath
-    
+
     unless exists $ do
         putStr $ "File " ++ configPath ++ " does not exist. Create new? [Y/n]: "
         answer <- getLine
         let response = map toLower answer
-        
+
         if (response /= "n") then do
           BL.writeFile configPath (encodePretty stdCfg)
           putStrLn $ "Created new config at: " ++ configPath
-        else 
+        else
           exitSuccess
 
     putStrLn $ "Using config: " ++ configPath
 
     file <- BL.readFile configPath
-    
-    case decode file of 
+
+    case decode file of
       Nothing  -> do
         putStrLn "Config parsing error"
-        exitFailure 
-      Just cfg -> do 
+        exitFailure
+      Just cfg -> do
         print cfg
-        execConfig cfg 
-
-
-
-
+        execConfig cfg
