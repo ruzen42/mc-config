37c874f253ab45e2c038f873df4fc93c818489e4
ruzen42 <malledeferlive@gmail.com>
2026-08-18 22:37:40 +0500

    fix

--- flake.nix
+++ flake.nix
@@ -1,64 +1,35 @@
 {
   inputs = {
-    nixpkgs.url      = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url  = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
-
-        haskell = pkgs.haskellPackages;
-
-        mc-config = haskell.mkDerivation {
-          pname   = "mc-config";
-          version = "1.2.0.0";
-          src     = ./.;
-
-          isLibrary    = false;
-          isExecutable = true;
-
-          executableHaskellDepends = with haskell; [
-            base
-            aeson
-            bytestring
-            wreq
-            lens
-            directory
-            process
-            optparse-applicative
-            rainbow
-            toml-parser
-            containers
-          ];
-
-          license     = pkgs.lib.licenses.mit;
-          maintainers = [ "Ruzen42" ];
-        };
-
-      in {
-        packages.default = mc-config;
-
-        apps.default = flake-utils.lib.mkApp {
-          drv = mc-config;
-        };
-
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
         devShells.default = pkgs.mkShell {
-          name = "mc-config-dev";
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+            stack
+            cabal-install
+            git
+          ];
 
           buildInputs = with pkgs; [
-            ghc
-            cabal-install
-            zlib
-            openssl
+            zlib 
+            haskell.compiler.native-bignum.ghc9103
           ];
 
-          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (with pkgs; [
-            zlib
-            openssl
-          ]);
+          shellHook = ''
+            export PKG_CONFIG_PATH="${pkgs.zlib.dev}/lib/pkgconfig:$PKG_CONFIG_PATH"
+            export CPATH="${pkgs.zlib.dev}/include:$CPATH"
+            export LIBRARY_PATH="${pkgs.zlib}/lib:$LIBRARY_PATH"
+          '';
         };
-      }
-    );
+
+        packages.default = pkgs.haskellPackages.callCabal2nix "mc-config" ./. {};
+      });
 }
